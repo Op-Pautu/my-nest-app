3c69bf83fed3d10165467a1369770bf643d855a2
OP <106511969+Op-Pautu@users.noreply.github.com>
2025-04-15 18:15:08 +0530

    refactor cats module and controller; add validation to CreateCatDto and global validation pipe

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,18 +1,18 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
-import { CatsService } from './cats.service';
+import { Body, Controller, Post, Get } from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
+import { CatsService } from './cats.service';
 
 @Controller('cats')
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
-  @Get()
-  findAll(): string {
-    return this.catsService.findAll();
+  @Post()
+  create(@Body() createCatDto: CreateCatDto) {
+    return this.catsService.create(createCatDto);
   }
 
-  @Post()
-  create(@Body() createCatDto: CreateCatDto): string {
-    return this.catsService.create(createCatDto);
+  @Get()
+  findAll() {
+    return this.catsService.findAll();
   }
 }
--- src/cats/dto/create-cat.dto.ts
+++ src/cats/dto/create-cat.dto.ts
@@ -1,5 +1,16 @@
+import { IsString, IsInt, MinLength, Min } from 'class-validator';
+import { Type } from 'class-transformer';
+
 export class CreateCatDto {
+  @IsString()
+  @MinLength(2)
   name: string;
+
+  @Type(() => Number)
+  @IsInt()
+  @Min(0)
   age: number;
+
+  @IsString()
   breed: string;
 }
